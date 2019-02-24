c15b1a7dd4438a8da1c8fe1a1b6e356763ed88e6
nevroje <joakim.berglind@gmail.com>
2019-02-24 17:05:13 +0100

    preventing merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
          has_many :orders, class_name: 'Cartify::Order', foreign_key: :user_id
          has_one :billing, class_name: 'Cartify::Billing', foreign_key: :user_id
          has_one :shipping, class_name: 'Cartify::Shipping', foreign_key: :user_id
